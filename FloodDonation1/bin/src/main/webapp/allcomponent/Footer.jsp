10420ce499774e0016bcb1f40b23938b8219d78e
Shubham Pandey <www.shubhamp1@gmail.com>
2023-09-10 09:02:57 +0530

    main

--- FloodDonation1/bin/src/main/webapp/allcomponent/Footer.jsp
+++
@@ -1,3 +0,0 @@
-<div class="fs-m container-fluid text-center p-2 text-white mt-2" style="background-color: #4a148c;">
-	<h5><i class="fa-solid fa-copyright me-2"></i>Design and Developed by Shubham</h5>
-</div>
--- FloodDonation1/src/main/java/com/example/demo/dao/MyOrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.MyOrder;
-
-public interface MyOrderRepo extends JpaRepository<MyOrder, Long> {
-	public MyOrder findByOrderId(String orderId); 
-}
